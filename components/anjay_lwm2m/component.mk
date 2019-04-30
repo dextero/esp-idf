978077f86987da5e8645a7bb38f346999d66f97c
Marcin Radomski <m.radomski@avsystem.com>
2019-04-30 16:42:16 +0200

    Add anjay_lwm2m component

---
+++ components/anjay_lwm2m/component.mk
@@ -0,0 +1,8 @@
+#
+# Component Makefile
+#
+
+COMPONENT_ADD_INCLUDEDIRS := Anjay/avs_commons/git/stream/include_public Anjay/avs_commons/git/stream/md5/include_public Anjay/avs_commons/git/stream/net/include_public Anjay/avs_commons/git/vector/include_public Anjay/avs_commons/git/http/include_public Anjay/avs_commons/git/rbtree/include_public Anjay/avs_commons/git/include_public Anjay/avs_commons/git/log/include_public Anjay/avs_commons/git/coap/include_public Anjay/avs_commons/git/compat/threading/include_public Anjay/avs_commons/git/utils/include_public Anjay/avs_commons/git/list/include_public Anjay/avs_commons/git/persistence/include_public Anjay/avs_commons/git/algorithm/include_public Anjay/avs_commons/git/net/include_public Anjay/avs_commons/git/buffer/include_public Anjay/avs_commons/git/sched/include_public Anjay/avs_commons/git/unit/include_public Anjay/include_public Anjay/modules/fw_update/include_public Anjay/modules/server/include_public Anjay/modules/attr_storage/include_public Anjay/modules/access_control/include_public Anjay/modules/security/include_public
+
+COMPONENT_SUBMODULES += Anjay
+
